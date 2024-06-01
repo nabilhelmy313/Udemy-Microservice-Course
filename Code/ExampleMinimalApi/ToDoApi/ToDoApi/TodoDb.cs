e5b53a709de3daade9e5c7a7dda76e806bcc9888
Nabil Helmy <nhelmy@arpuplus.com>
2024-06-01 23:37:00 +0300

    make-empty-minimalappandtodoapp

---
+++ Code/ExampleMinimalApi/ToDoApi/ToDoApi/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi;
+
+public class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<TodoItem> Todos { get; set; }
+
+}
