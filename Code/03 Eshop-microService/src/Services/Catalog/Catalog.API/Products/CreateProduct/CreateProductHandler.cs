3379b67e7f745cc0621831ae77e7022ccd9a4817
Nabil Helmy <nhelmy@arpuplus.com>
2024-06-04 03:15:14 +0300

    createwithmarten

--- Code/03 Eshop-microService/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ Code/03 Eshop-microService/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿
+namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(Guid Id,
+      string Name,
+      List<string> Category,
+      string Description,
+      string ImageFile,
+      decimal Price):ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler (IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public class CreateProductHandler
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        //create Product 
+        var product = new Product()
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        };
+        //TODO
+        //save to DB
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        //retrun the result
+        return new CreateProductResult(product.Id);
     }
 }
--- Code/03 Eshop-microService/src/Services/Catalog/Catalog.API/Program.cs
+++ Code/03 Eshop-microService/src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 //Di Services  To Container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config => config.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 var app = builder.Build();
 //Configure the http request
-
+app.MapCarter();
 app.Run();
