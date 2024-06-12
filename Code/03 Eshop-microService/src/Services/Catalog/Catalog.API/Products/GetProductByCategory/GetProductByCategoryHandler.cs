e24ecad40cc601de64cb235651b831191acc1363
Nabil Helmy <nhelmy@arpuplus.com>
2024-06-12 05:09:57 +0300

    makegets

---
+++ Code/03 Eshop-microService/src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string Category):IQuery<GetProductByCategoryResult>;
+public record  GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryQueryHandler
+    (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+
+{
+    public  async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+
+        logger.LogInformation($"GetProductByCategoryQueryHandler called with {query}");
+        var products= await session.Query<Product>()
+            .Where(a=>a.Category.Contains(query.Category)).ToListAsync();
+        return new GetProductByCategoryResult(products);
+    }
+}
